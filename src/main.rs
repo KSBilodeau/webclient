09b3b64e4f8dd4e4fcb335a7e0beabc6c0ee86db
KSBilodeau <22333708+KSBilodeau@users.noreply.github.com>
2025-03-05 18:13:19 -0600

    Replace unnecessary usage of `bail` with `with_context`

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,16 @@
-use anyhow::bail;
+use anyhow::{Context, bail};
 use std::net::TcpStream;
 
 fn main() -> anyhow::Result<()> {
     // RETRIEVE SERVER INFORMATION FROM ENVIRONMENT VARIABLES
 
-    let Ok(ip_addr) = std::env::var("SERVER_IP") else {
-        bail!("Missing server IP address envvar")
-    };
-
-    let Ok(ip_port) = std::env::var("SERVER_PORT") else {
-        bail!("Missing server port envvar")
-    };
+    let ip_addr = std::env::var("SERVER_IP").with_context(|| "Missing server IP address envvar")?;
+    let ip_port = std::env::var("SERVER_PORT").with_context(|| "Missing server port envvar")?;
 
     // CONNECT THE CLIENT TO THE GIVEN IP ADDRESS AND PORT
 
-    let Ok(mut stream) = TcpStream::connect(format!("{ip_addr}:{ip_port}")) else {
-        bail!("Failed to connect to the server")
-    };
+    let mut stream = TcpStream::connect(format!("{ip_addr}:{ip_port}"))
+        .with_context(|| "Failed to connect to the server")?;
 
     // GENERATE THE RSA KEY PAIR FOR MESSAGE ENCRYPTION
 
@@ -24,7 +18,8 @@ fn main() -> anyhow::Result<()> {
 
     // EXCHANGE KEYS WITH SERVER
 
-    let client_public_key = webutils::exchange_keys(&key_pair.public_key, &mut stream)?;
+    let client_public_key = webutils::exchange_keys(&key_pair.public_key, &mut stream)
+        .with_context(|| "Failed to exchange keys with server")?;
 
     // CONFIRM KEYS WERE SUCCESSFULLY SWAPPED
 
@@ -33,7 +28,7 @@ fn main() -> anyhow::Result<()> {
         &key_pair.private_key,
         &mut stream,
         b"ACK",
-    )?;
+    ).with_context(|| "Failed to send message to the server")?;
 
     if ack != "ACK" {
         bail!("ACKNOWLEDGEMENT FAILED")
