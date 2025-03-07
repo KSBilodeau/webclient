5b26ebc5ad6898b323f34ab225d78c7744958dc5
KSBilodeau <22333708+KSBilodeau@users.noreply.github.com>
2025-03-07 13:02:49 -0600

    Add connection logging for client

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,24 @@ fn main() -> anyhow::Result<()> {
 
     // CONNECT THE CLIENT TO THE GIVEN IP ADDRESS AND PORT
 
+    println!("Connecting to server...");
     let mut stream = TcpStream::connect(format!("{ip_addr}:{ip_port}"))
         .with_context(|| "Failed to connect to the server")?;
 
     // GENERATE THE RSA KEY PAIR FOR MESSAGE ENCRYPTION
 
+    println!("Generating key pair...");
     let key_pair = webutils::generate_key_pair()?;
 
     // EXCHANGE KEYS WITH SERVER
 
+    println!("Exchanging keys with the server...");
     let server_public_key = webutils::exchange_keys(&key_pair.public_key, &mut stream)
         .with_context(|| "Failed to exchange keys with server")?;
 
     // CONFIRM KEYS WERE SUCCESSFULLY SWAPPED
 
+    println!("Synchronizing with the server...");
     webutils::synchronize(&server_public_key, &key_pair.private_key, &mut stream)
         .with_context(|| "Failed to synchronize with server")?;
 
@@ -35,7 +39,7 @@ fn main() -> anyhow::Result<()> {
             &mut stream,
             b"HEARTBEAT",
         )
-        .with_context(|| "Failed to synchronize heartbeat")?;
+            .with_context(|| "Failed to synchronize heartbeat")?;
         println!(
             "HEARTBEAT SYNCHRONIZED [Time since epoch: {:.2?}]",
             std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH)?
